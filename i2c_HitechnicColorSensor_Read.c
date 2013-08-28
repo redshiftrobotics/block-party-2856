dfb05ac529838f541ceb6fbacebb6e4ea7841695
Isaac Benson Zinda <isaaczinda@seattleacademy.org>
2013-08-28 15:40:05 -0700

    Make i2c color sensor actually work

--- i2c_HitechnicColorSensor_Read.c
+++ i2c_HitechnicColorSensor_Read.c
@@ -7,20 +7,38 @@ task main()
 
 	tByteArray I2Crequest;
 	tByteArray I2Cresponse;
-	ubyte I2Caddress = 0x02; //standard address for Lego sensor
 
-	I2Crequest[0] = 2;
+	//set sets the adress of the byte to the default adress for sending data
+	ubyte I2Caddress = 0x02;
+
+	//sets the number of bytes to send:
+	I2Crequest[0] = 4;
+
+	//sends the adress as the first byte
   I2Crequest[1] = I2Caddress;
-  I2Crequest[2] = 0x42;  //standard Lego Register address
+
+  //sets the starting position to start sending data at
+  I2Crequest[2] = 0x46;
+
+  //sets the motor power
+  I2Crequest[3] = 0b00010000;
+
+  //sets the motor mode
+  I2Crequest[4] = 0b00000001;
+
 
   while(true)
   {
-  	eraseDisplay();
+  	//sends the command to the motor controller in a loop
   	writeI2C(S1, I2Crequest, I2Cresponse, 4);
-  	nxtDisplayString(3, "%i", I2Cresponse[0]);  // color
-  	nxtDisplayString(4, "%i", I2Cresponse[1]);  // red component
-  	nxtDisplayString(5, "%i", I2Cresponse[2]);  // green component
-  	nxtDisplayString(6, "%i", I2Cresponse[3]);	// blue component
-  	wait1Msec(10);
+
+  	//erases the display and writes the responses
+  	eraseDisplay();
+  	nxtDisplayString(0, "%i", I2Cresponse[0]);
+  	nxtDisplayString(1, "%i", I2Cresponse[1]);
+  	nxtDisplayString(2, "%i", I2Cresponse[2]);
+  	nxtDisplayString(3, "%i", I2Cresponse[3]);
+  	nxtDisplayString(4, "%i", I2Cresponse[4]);
+  	Sleep(10);
 	}
 }
