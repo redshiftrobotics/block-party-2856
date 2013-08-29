b3f750b11f0257def2bdffa2089c9b944c5c6b66
Alex Jordan <alexander3223098@gmail.com>
2013-08-29 16:57:51 -0700

    initial move of the i2c motor stuff to a library

--- i2c_DCMotorController_MoveToPosition.c
+++ i2c_DCMotorController_MoveToPosition.c
@@ -1,59 +1,7 @@
 #pragma config(Sensor, S1,     M_CONTROL,      sensorI2CCustom)
 
 #include "./drivers/common.h"
-
-long EncoderValue()
-{
-		//initializes the arrays
-		tByteArray I2Crequest;
-		tByteArray I2Cresponse;
-
-		//sets the number of bytes to send:
-		I2Crequest[0] = 2;
-
-		//sends the adress as the first byte
-	  I2Crequest[1] = 0x02;
-
-	  //sets the starting position to start sending data at
-	  I2Crequest[2] = 0x50;
-
-	  //writes the data, and gets the response
-	  writeI2C(S1, I2Crequest, I2Cresponse, 4);
-
-	  //creates a long out of the bytes
-	  long EncoderValue = (I2Cresponse[0] << 24) + (I2Cresponse[1] << 16) + (I2Cresponse[2] << 8) + (I2Cresponse[3] << 0);
-
-	  //returns the long
-  	return EncoderValue;
-}
-
-void SetEncoderPosition(long Input)
-{
-	//initializes the arrays
-	tByteArray I2Crequest;
-
-	//sets the number of bytes to send:
-	I2Crequest[0] = 8;
-
-	//sends the adress as the first byte
-  I2Crequest[1] = 0x02;
-
-	//sets the starting position to start sending data at
-  I2Crequest[2] = 0x46;
-
-  //sets the motor power
-  I2Crequest[3] = 0b00010000;
-
-  //sets the motor mode
-  I2Crequest[4] = 0b00000010;
-
-  I2Crequest[5] = (byte)((Input >> 24) & 0x000000ff);
-  I2Crequest[6] = (byte)((Input >> 16) & 0x000000ff);
-  I2Crequest[7] = (byte)((Input >> 8) & 0x000000ff);
-  I2Crequest[8] = (byte)(Input & 0x000000ff);
-
-  writeI2C(S1, I2Crequest);
-}
+#include "libraries/libi2cmotor.h"
 
 //void RotateDegrees(bool DisplayTargetPosition, bool DisplayCurrentPosition, long TargetPosition)
 //{
@@ -62,17 +10,25 @@ void SetEncoderPosition(long Input)
 //}
 
 
+
 task main()
 {
-	int TargetPosition = EncoderValue() + 1440;
+	int TargetPosition = getEncoderValue(2, S1) + 1440;
 
-	SetEncoderPosition(TargetPosition);
+	setEncoderPosition(2, S1, TargetPosition);
 
   while(true)
   {
   	eraseDisplay();
   	nxtDisplayString(0, "%i", TargetPosition);
-  	nxtDisplayString(1, "%i", EncoderValue());
+  	nxtDisplayString(1, "%i", getEncoderValue(2, S1));
+  	if (isBusy(2, S1))
+  		{
+  			nxtDisplayString(2, "Still working, yo!");
+  		} else {
+  			nxtDisplayString(2, "Done!");
+  		}
   	Sleep(10);
+
 	}
 }
---
+++ libraries/libi2cmotor.h
@@ -0,0 +1,104 @@
+#include "drivers/common.h"
+
+// doesn't support daisychains
+// also TODO: setting a constant speed and a rotating a certain amount
+
+int i2cmotor_lastMotor = null;
+tSensors i2cmotor_lastPort = null;
+
+// pass this 1 or 2 for the motor and S[1-4] for the port
+long getEncoderValue(int motor, tSensors port)
+{
+		i2cmotor_lastMotor = motor;
+		i2cmotor_lastPort = port;
+		//initializes the arrays
+		tByteArray I2Crequest;
+		tByteArray I2Cresponse;
+
+		//sets the number of bytes to send:
+		I2Crequest[0] = 2;
+
+		//sends the adress as the first byte
+	  I2Crequest[1] = 0x02;
+
+	  //sets the starting position to start sending data at
+	  if (motor == 1)
+	  {
+	  	I2Crequest[2] = 0x49;
+		} else {
+		  I2Crequest[2] = 0x50;
+		}
+
+	  //writes the data, and gets the response
+	  writeI2C(port, I2Crequest, I2Cresponse, 4);
+
+	  //creates a long out of the bytes
+	  long EncoderValue = (I2Cresponse[0] << 24) + (I2Cresponse[1] << 16) + (I2Cresponse[2] << 8) + (I2Cresponse[3] << 0);
+
+	  //returns the long
+  	return EncoderValue;
+}
+
+// motor should be 1 or 2, port should be S[1-4], Input should be the position to move to
+void setEncoderPosition(int motor, tSensors port, long Input)
+{
+	//initializes the arrays
+	tByteArray I2Crequest;
+
+	//sets the number of bytes to send:
+	I2Crequest[0] = 8;
+
+	//sends the adress as the first byte
+  I2Crequest[1] = 0x02;
+
+	//sets the starting position to start sending data at
+  I2Crequest[2] = 0x46;
+
+  //sets the motor power
+  I2Crequest[3] = 0b00010000;
+
+  //sets the motor mode
+  I2Crequest[4] = 0b00000010;
+
+  I2Crequest[5] = (byte)((Input >> 24) & 0x000000ff);
+  I2Crequest[6] = (byte)((Input >> 16) & 0x000000ff);
+  I2Crequest[7] = (byte)((Input >> 8) & 0x000000ff);
+  I2Crequest[8] = (byte)(Input & 0x000000ff);
+
+  writeI2C(S1, I2Crequest);
+}
+
+// see getEncoderValue() for args
+bool isBusy(int motor, tSensors port)
+{
+	tByteArray I2Crequest;
+	tByteArray I2Cresponse;
+
+	ubyte busy;
+
+	// number of additional bytes to send
+	I2Crequest[0] = 2;
+
+	// standard lego sensor address
+	I2Crequest[1] = 0x02;
+
+	// pin address for either motor 1 or motor 2
+	if (motor == 1)
+	{
+		I2Crequest[2] = 0x44;
+	} else {
+		I2Crequest[2] = 0x47;
+	}
+
+	// ask on the port "port" for the data in I2Crequest. get back 1 byte and put it in I2Cresponse.
+	writeI2C(port, I2Crequest, I2Cresponse, 1);
+
+	// we want busy to be only the leftmost bit in the response, so we bitshift
+	busy = (I2Cresponse[0] >> 7);
+
+	if (busy == 0x01) {
+		return true;
+	} else {
+		return false;
+	}
+}
