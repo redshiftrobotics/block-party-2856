c00866f64ee2c7dd208e013cfbfb1af3edaf5552
Alec Guthrie <alexanderg100@gmail.com>
2013-10-29 17:47:17 -0700

    Quick Additions and Header Documentation at the top of IrLobf.h

--- libraries/IrLobf.h
+++ libraries/IrLobf.h
@@ -1,6 +1,9 @@
 #pragma config(Sensor, S1,     IR,             sensorI2CCustom)
 #include "HitecnicInfaredLibrary.h"
-
+/*
+SYNTAX: "slopeLOBF(20,false)" will call for the IR Seeker V2 to take 20 readings and not display any information to the debug screen.
+USAGE: Compare slopeLOBF to a threshold value. If the robot is moving towards the IR beacon, the value of slopeLOBF will be positive. When moving away, slopeLOBF is negative.
+*/
 int acS1, acS2, acS3, acS4, acS5 = 0;
 
 long sY = 0; //We are squaring summations -> KEEP THESE AS LONGS
@@ -41,7 +44,6 @@ float slopeLOBF (int n, bool debug) //"n" is your sample size and WILL INCREASE 
 	sXsY = (sX*sY);
 	msXsY = (sXsY)/n;
 	LOBF_slope = (spXYp - msXsY)/(spX2p - mpsXp2);
-	return LOBF_slope;
 
 	if (debug == true)
 	{
@@ -54,5 +56,5 @@ float slopeLOBF (int n, bool debug) //"n" is your sample size and WILL INCREASE 
 		writeDebugStream("msXsY:%f, ", msXsY);
 		writeDebugStreamLine("spXYp:%i", spXYp);
 	}
-	Sleep(50);
+	return LOBF_slope;
 }
