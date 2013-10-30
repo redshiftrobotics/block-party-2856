aed455b41f47448386f42379c5b1eb2d29b79720
Alec Guthrie <alexanderg100@gmail.com>
2013-10-29 17:12:23 -0700

    Created a header for my LOBF formula. Also added its included header into the libraries folder. Also made an autonomous to test using my IrLobf.h header with issacs Motors.h header.

---
+++ IR/IrLobfAuto.c
@@ -0,0 +1,21 @@
+#pragma config(Sensor, S1,     IR,             sensorI2CCustom)
+#include Motors.h
+#IrLobf.h
+task main()
+{
+	while (true)
+	{
+		while (slopeLOBF(20,false) > 0)
+		{
+			Motors_SetSpeed(S1,1,1,50);
+			Motors_SetSpeed(S1,1,2,50);
+			Motors_SetSpeed(S1,2,1,-50);
+			Motors_SetSpeed(S1,2,2,-50);
+		}
+
+		Motors_SetSpeed(S1,1,1,0);
+		Motors_SetSpeed(S1,1,2,0);
+		Motors_SetSpeed(S1,2,1,0);
+		Motors_SetSpeed(S1,2,2,0);
+	}
+}
---
+++ libraries/IrLobf.h
@@ -0,0 +1,58 @@
+#pragma config(Sensor, S1,     IR,             sensorI2CCustom)
+#include "HitecnicInfaredLibrary.h"
+
+int acS1, acS2, acS3, acS4, acS5 = 0;
+
+long sY = 0; //We are squaring summations -> KEEP THESE AS LONGS
+long sX = 0;
+long spX2p = 0;
+long psXp2 = 0;
+float mpsXp2 = 0;
+long sXsY = 0;
+float msXsY = 0;
+long spXYp = 0;
+
+float LOBF_slope = 0;
+
+float slopeLOBF (int n, bool debug) //"n" is your sample size and WILL INCREASE THE TIME YOU ENCONTER SLEEP FUNCTIONS
+{
+	if (debug == true) {clearDebugStream();}
+	
+	sY = 0;
+	sX = 0;
+	spXYp = 0;
+	spX2p = 0;
+	for (int X = 1; X<n+1 ; X++)
+	{
+		HTIRS2readAllACStrength(IR, acS1, acS2, acS3, acS4, acS5);
+		sY += acS3;
+		sX += X;
+		spX2p += X*X;
+		spXYp += (X * acS3) ;
+		if (debug == true)
+		{
+			writeDebugStream("%i,", acS3);
+			writeDebugStreamLine("0");
+		}
+		Sleep(10);
+	}
+	psXp2 = (sX*sX);
+	mpsXp2 = psXp2/n;
+	sXsY = (sX*sY);
+	msXsY = (sXsY)/n;
+	LOBF_slope = (spXYp - msXsY)/(spX2p - mpsXp2);
+	return LOBF_slope
+	
+	if (debug == true)
+	{
+		writeDebugStream("0, ");
+		writeDebugStream("slope:%f, ", slopeLOBF);
+		writeDebugStream("sY:%i, ", sY);
+		writeDebugStream("psXp2:%i, ", psXp2);
+		writeDebugStream("mpsXp2:%f, ", mpsXp2);
+		writeDebugStream("sXsY:%i, ", sXsY);
+		writeDebugStream("msXsY:%f, ", msXsY);
+		writeDebugStreamLine("spXYp:%i", spXYp);
+	}
+	Sleep(50);
+}
