b0b38e69746304c3d2c2ff7de427a4c496a9fbd6
Alex Jordan <alexander3223098@gmail.com>
2013-11-01 18:25:25 -0700

    almost finish up the synchronous task queue
    this will be the canonical I2C implementation from now on.

--- libraries/Motors.h
+++ libraries/Motors.h
@@ -1,94 +1,46 @@
 #include "I2C.h"
 
-int Motors_Length = 0;
+// TODO: add the final method to send the bus messages from the job register
 
-tSensors Motors_Port[10];
-int Motors_DaisyChainLevel[10];
-int Motors_MotorNumber[10];
-byte Motors_Mode[10];
+/*
+	Table of motor mode numbers:
+	0: Move by encoder count
+	1: Move by speed
+*/
 
+int Motors_MotorMovementMode[32] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+long Motors_MotorMovementNumber[32] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
-byte Motors_ModeByLocation(tSensors Port, int DaisyChainLevel, int MotorNumber)
+// internal method, do not use
+// for a given motor, gets a unique id throughout the entire robot
+int _Motors_GetCanonicalMotorId(tSensors SensorPort, int DaisyChainLevel, int MotorNumber)
 {
-	for(int i = 0; i < Motors_Length; i++)
-	{
-		if(Port == Motors_Port[i] && DaisyChainLevel == Motors_DaisyChainLevel[i] && MotorNumber == Motors_MotorNumber[i])
-		{
-			return Motors_Mode[i];
-		}
-	}
-	return 0b00000000;
-}
+	// initially assume the id is 0. it will be manipulated from here.
+	int id = 0;
 
-int Motors_ContainsMode(tSensors Port, int DaisyChainLevel, int MotorNumber)
-{
-	for(int i = 0; i < Motors_Length; i++)
-	{
-		if(Port == Motors_Port[i] && DaisyChainLevel == Motors_DaisyChainLevel[i] && MotorNumber == Motors_MotorNumber[i])
-		{
-			return i;
-		}
-	}
-	return -1;
-}
+	// get a port id that we can work with
+	int port = _I2C_GetIntFromPort(SensorPort);
 
-void Motors_NewMode(tSensors Port, int DaisyChainLevel, int MotorNumber, byte Mode)
-{
-	I2C_SetMode(Port, DaisyChainLevel, MotorNumber, Mode);
+	// add in the daisychain level information
+	id += (DaisyChainLevel-1)*2;
 
-	int IndexOfList = Motors_ContainsMode(Port, DaisyChainLevel, MotorNumber);
-	if(IndexOfList != -1)
-	{
-		Motors_Mode[IndexOfList] = Mode;
-	}
-	else
-	{
-		Motors_DaisyChainLevel[Motors_Length] = DaisyChainLevel;
-		Motors_MotorNumber[Motors_Length] = MotorNumber;
-		Motors_Port[Motors_Length] = Port;
-		Motors_Mode[Motors_Length] = Mode;
-		Motors_Length++;
-	}
+	// add in the motor information
+	id += MotorNumber-1;
+
+	// add in the port information
+	id += (port-1)*8;
 }
 
 void Motors_SetSpeed(tSensors Port, int DaisyChainLevel, int MotorNumber, int Speed)
 {
-	if(MotorNumber == 1)
-	{
-		Motors_NewMode(Port, DaisyChainLevel, 1, 0b00010001);
-		Motors_NewMode(Port, DaisyChainLevel, 2, Motors_ModeByLocation(Port, DaisyChainLevel, 2) | 0b00010000);
-
-		I2C_SetMotorSpeed(Port, DaisyChainLevel, MotorNumber, Speed);
-	}
-	else if (MotorNumber == 2)
-	{
-		Motors_NewMode(Port, DaisyChainLevel, 1, Motors_ModeByLocation(Port, DaisyChainLevel, 1) | 0b00010000);
-		Motors_NewMode(Port, DaisyChainLevel, 2, 0b00010001);
-
-		I2C_SetMotorSpeed(Port, DaisyChainLevel, MotorNumber, Speed);
-	}
+	// TODO: possible typecasting problem of int Speed to long Motors_MotorMovementNumber
+	Motors_MotorMovementMode[_Motors_GetCanonicalMotorId(Port, DaisyChainLevel, MotorNumber)] = 1;
+	Motors_MotorMovementNumber[_Motors_GetCanonicalMotorId(Port, DaisyChainLevel, MotorNumber)] = Speed;
 }
 
 void Motors_SetPosition(tSensors Port, int DaisyChainLevel, int MotorNumber, long MotorEncoder, int Speed)
 {
-	if(MotorNumber == 1)
-	{
-		Motors_NewMode(Port, DaisyChainLevel, 1, 0b00010010);
-		Motors_NewMode(Port, DaisyChainLevel, 2, Motors_ModeByLocation(Port, DaisyChainLevel, 2) | 0b00010000);
-
-		I2C_SetEncoderPosition(Port, DaisyChainLevel, MotorNumber, MotorEncoder, Speed);
-	}
-	else if (MotorNumber == 2)
-	{
-		Motors_NewMode(Port, DaisyChainLevel, 1, Motors_ModeByLocation(Port, DaisyChainLevel, 1) | 0b00010000);
-		Motors_NewMode(Port, DaisyChainLevel, 2, 0b00010010);
-
-		I2C_SetEncoderPosition(Port, DaisyChainLevel, MotorNumber, MotorEncoder, Speed);
-	}
-}
-
-void Motors_MoveRotations(tSensors Port, int DaisyChainLevel, int MotorNumber, float Rotations, int Speed)
-{
-
-	Motors_SetPosition(Port, DaisyChainLevel, MotorNumber, I2C_GetEncoderPosition(Port, DaisyChainLevel, MotorNumber) + (Rotations * 1440), Speed);
+	// TODO: the Speed argument is silently dropped
+	Motors_MotorMovementMode[_Motors_GetCanonicalMotorId(Port, DaisyChainLevel, MotorNumber)] = 0;
+	Motors_MotorMovementNumber[_Motors_GetCanonicalMotorId(Port, DaisyChainLevel, MotorNumber)] = MotorEncoder;
 }
