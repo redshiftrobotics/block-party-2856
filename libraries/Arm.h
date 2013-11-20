0ba233bc5ee8681aba0b7ef389ec055b3cb25f4d
poxls88 <alexanderg100@gmail.com>
2013-11-19 17:01:21 -0800

    Arm.h supports TouchMux/multiple touch sensors. When ready to implement the changes, uncomment the changes in the code.

--- libraries/Arm.h
+++ libraries/Arm.h
@@ -1,5 +1,6 @@
 #pragma config(Sensor, S1,     ,               sensorI2CCustom)
-#pragma config(Sensor, S4,     ,               sensorHiTechnicTouchMux)
+#pragma config(Sensor, S4,     ,               sensorTouch)
+//#pragma config(Sensor, S4,     ,               sensorHiTechnicTouchMux)
 
 #include "I2C.h"
 
@@ -9,7 +10,7 @@ int Arm_BottomEncoderLimit = 0;
 int Arm_TopEncoderLimit = 0;
 int Arm_Encoder = 0;
 int Arm_Speed = 0;
-int nMux;
+int nMux = 0;
 
 void Arm_SetSpeed(int Speed)
 {
@@ -23,16 +24,18 @@ void Arm_SetSpeed(int Speed)
 void Arm_Update()
 {
 	Arm_Encoder = I2C_GetEncoderPosition(S1, 1, 1);
-	
+
 	//if the arm isn't initialized yet
 	if(!Arm_Initialized)
 	{
-		int nMux = SensorValue[S4] //Values from sensorHitechnicTouchMux is a 4-bit map for each of possible four touch sensors ------>
+		nMux = SensorValue[S4]; //Values from sensorHitechnicTouchMux is a 4-bit map for each of possible four touch sensors
 		//set the speed to 10
 		Arm_Speed = 10;
 
 		//if the lower limit is triggered, set the upper and lower encoder bounds
-		if(nMux & 0x01) //make sure to plug the lower sensor into port 1 ------------------------------------------------------------->
+		//nMux should be bitwise added to 0x01 for sensor 1, use 0x02 for sensor 2, use 0x04 for sensor 3, use 0x08 for sensor 4
+		//if(nMux & 0x01) //make sure to plug the lower sensor into port 1
+		if(SensorValue[S4] == 1) //replace this if line with the if line above (uncommented obviously)
 		{
 			Arm_Initialized = true;
 			Arm_Speed = 0;
@@ -45,14 +48,14 @@ void Arm_Update()
 	//if the arm is initialized
 	else
 	{
-		int nMux = SensorValue[S4]
+		nMux = SensorValue[S4];
 		//if the arm is with in the encoder range
 		if(Arm_Encoder < Arm_TopEncoderLimit && Arm_Encoder > Arm_BottomEncoderLimit)
 		{
-			//and the arm is within the upper bound. Comment this in when we are ready to test it--------------------------------->
+			//And the arm is within the upper bound
 			//if(nMux & 0x02) //make sure to plug the top limit switch into port #2
 			//{
-			I2C_SetMotorSpeed(S1, 1, 1, Arm_Speed);
+				I2C_SetMotorSpeed(S1, 1, 1, Arm_Speed);
 			//}
 		}
 
